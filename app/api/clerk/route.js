3b778941e3256e83eaa613b7ffc2247a91864403
Anish <anish@Anishs-MacBook-Air.local>
2026-01-29 22:43:39 +0530

    mongodb fixed problem

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -22,7 +22,7 @@ export async function POST(req) {
 
     const userData = {
         _id: data.id,
-        email:data.email_addresses[0].email_addresses,
+        email: data.email_addresses[0].email_address,
         name: `${data.first_name} ${data.last_name}`,
         image: data.image_url,
     }
